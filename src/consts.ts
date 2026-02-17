ddedcf3b53cdd8463a836d42ef6ab8a66f3ce579
rachmadhartono78 <rahmadhartono78@gmail.com>
2026-02-17 21:35:20 +0700

    feat: add initial site structure including home and about pages, footer component, and site constants.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Rachmad\'s Blog';
+export const SITE_TITLE = 'Rachmad Hartono\'s Blog';
 export const SITE_DESCRIPTION = 'Database Administrator, Software Engineer, Business, and Property Reviews in Jogja.';
